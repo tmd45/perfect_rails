2f3c793f2b84d759d0d8e2cc5ea6f4f023334f07
tmd45 <tmd45@otegami.jpn.org>
2015-08-16 17:02:04 +0900

    Add routes member resources and collection resources to publishers

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   get "/books/:id" => "books#show"
-  resources :publishers
+  resources :publishers do
+    resources :books
+
+    member do
+      get 'detail'
+    end
+
+    collection do
+      get 'search'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
