6cc953729d99d0effc9ebcd8204b58c6cca85a14
tmd45 <tmd45@otegami.jpn.org>
2015-08-16 17:23:11 +0900

    $ ./bin/rails g controller profiles show edit update

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
+  get 'profiles/edit'
+
+  get 'profiles/update'
+
   resource :profile, only: %i{show edit update}
 
   get "/books/:id" => "books#show"
