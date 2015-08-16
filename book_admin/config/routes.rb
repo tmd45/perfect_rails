bb353ebc11d4b0178a3e99f77bddcc4d422e98e6
tmd45 <tmd45@otegami.jpn.org>
2015-08-16 17:07:25 +0900

    $ ./bin/rails g controller Profile show edit update

--- book_admin/config/routes.rb
+++ book_admin/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'profile/show'
+
+  get 'profile/edit'
+
+  get 'profile/update'
+
   get "/books/:id" => "books#show"
   resources :publishers do
     resources :books
