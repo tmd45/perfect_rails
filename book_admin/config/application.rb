c4f24dfaf5d9f2c73b9ff2813efa461419d12488
tmd45 <tmd45@otegami.jpn.org>
2015-08-19 00:23:46 +0900

    Example sprockets add assets path

--- book_admin/config/application.rb
+++ book_admin/config/application.rb
@@ -19,5 +19,8 @@ module BookAdmin
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Sprockets add assets path
+    # config.assets.path << Rails.root.join("components")
   end
 end
