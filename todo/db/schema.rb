c13f7147ef2f1dc4cdd73ef4e275debf9542f0a3
tmd45 <tmd45@otegami.jpn.org>
2015-09-11 00:23:35 +0900

    $ ./bin/rake db:migrate

--- todo/db/schema.rb
+++ todo/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813162639) do
+ActiveRecord::Schema.define(version: 20150910152237) do
 
   create_table "tasks", force: true do |t|
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "family_name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
