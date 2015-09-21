4b43d4aec7159a05f548875b3db2f4484be77c21
tmd45 <tmd45@otegami.jpn.org>
2015-09-21 12:18:21 +0900

    Add login to Twitter and register user

--- awesome_events/app/controllers/sessions_controller.rb
+++ awesome_events/app/controllers/sessions_controller.rb
@@ -1,2 +1,8 @@
 class SessionsController < ApplicationController
+  # get '/auth/:provider/callback'
+  def create
+    user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to root_path, notice: 'ログインしました'
+  end
 end
--- awesome_events/app/models/user.rb
+++ awesome_events/app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  # Find or Create from OmniAuth::AuthHash
+  #
+  # @param auth_hash [OmniAuth::AuthHash]
+  # @return user
+  def self.find_or_create_from_auth_hash(auth_hash)
+    provider  = auth_hash[:provider]
+    uid       = auth_hash[:uid]
+    nickname  = auth_hash[:info][:nickname]
+    image_url = auth_hash[:info][:image]
+
+    User.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.nickname = nickname
+      user.image_url = image_url
+    end
+  end
 end
