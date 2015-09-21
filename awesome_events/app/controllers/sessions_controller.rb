3988cc64e122b141261e338fb39244aaf2ad4e95
tmd45 <tmd45@otegami.jpn.org>
2015-09-21 12:27:12 +0900

    Add logout

--- awesome_events/app/controllers/sessions_controller.rb
+++ awesome_events/app/controllers/sessions_controller.rb
@@ -5,4 +5,10 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path, notice: 'ログインしました'
   end
+
+  # get '/logout'
+  def destroy
+    reset_session
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
 end
