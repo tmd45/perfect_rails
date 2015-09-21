ebee067e9f4e0643e19fab2e90c6cec5c154fa4a
tmd45 <tmd45@otegami.jpn.org>
2015-09-21 15:47:59 +0900

    Add restrictions to migrate create tickets

--- awesome_events/db/migrate/20150921064256_create_tickets.rb
+++ awesome_events/db/migrate/20150921064256_create_tickets.rb
@@ -1,11 +1,14 @@
 class CreateTickets < ActiveRecord::Migration
   def change
     create_table :tickets do |t|
-      t.references :user, index: true
-      t.references :event, index: true
+      t.references :user
+      t.references :event, null: false
       t.string :comment
 
       t.timestamps
     end
+
+    add_index :tickets, [ :user_id, :event_id ], unique: true, name: :index_user_id_event_id
+    add_index :tickets, [ :event_id, :user_id ], unique: true, name: :index_event_id_user_id
   end
 end
