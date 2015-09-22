0834ec6c3931f4a57babfcb94f1ac738ae9c3818
tmd45 <tmd45@otegami.jpn.org>
2015-09-22 18:47:40 +0900

    Add spec for Event#name validations

--- awesome_events/spec/models/event_spec.rb
+++ awesome_events/spec/models/event_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    context 'has nil' do
+      let(:event) { Event.new(name: nil) }
+
+      it 'is invalid' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
+    context 'has blank' do
+      let(:event) { Event.new(name: '') }
+
+      it 'is invalid' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
+    context 'has "Rails勉強会"' do
+      let(:event) { Event.new(name: 'Rails勉強会') }
+
+      it 'is valid' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context 'has 50 characters' do
+      let(:event) { Event.new(name: 'a' * 50) }
+
+      it 'is valid' do
+        event.valid?
+        expect(event.errors[:name]).to be_blank
+      end
+    end
+
+    context 'has 51 characters' do
+      let(:event) { Event.new(name: 'a' * 51) }
+
+      it 'is invalid' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+  end
 end
