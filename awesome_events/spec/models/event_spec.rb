91af5ae9530748e99c09d84afd1a91b603665bbf
tmd45 <tmd45@otegami.jpn.org>
2015-09-22 18:56:31 +0900

    Use shoulda-matchers for validation specs

--- awesome_events/spec/models/event_spec.rb
+++ awesome_events/spec/models/event_spec.rb
@@ -2,49 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe '#name' do
-    context 'has nil' do
-      let(:event) { Event.new(name: nil) }
-
-      it 'is invalid' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-
-    context 'has blank' do
-      let(:event) { Event.new(name: '') }
-
-      it 'is invalid' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-
-    context 'has "Rails勉強会"' do
-      let(:event) { Event.new(name: 'Rails勉強会') }
-
-      it 'is valid' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-
-    context 'has 50 characters' do
-      let(:event) { Event.new(name: 'a' * 50) }
-
-      it 'is valid' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-
-    context 'has 51 characters' do
-      let(:event) { Event.new(name: 'a' * 51) }
-
-      it 'is invalid' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(50) }
   end
 end
