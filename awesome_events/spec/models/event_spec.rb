212abb95daa2d591f71cfb681acee3c0fb3ac71f
tmd45 <tmd45@otegami.jpn.org>
2015-09-22 19:41:58 +0900

    Add spec Event#created_by? (exercise of using fixture)

--- awesome_events/spec/models/event_spec.rb
+++ awesome_events/spec/models/event_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe Event, type: :model do
       it { should validate_length_of(:name).is_at_most(50) }
     end
   end
+
+  describe '#created_by?' do
+    let(:event) { create(:event) }
+    subject { event.created_by?(user) }
+
+    context 'has parameter is nil' do
+      let(:user) { nil }
+      it { should be_falsey }
+    end
+
+    context "has #owner_id same parameter#id" do
+      let(:user) { double('user', id: event.owner_id) }
+      it { should be_truthy }
+    end
+  end
 end
