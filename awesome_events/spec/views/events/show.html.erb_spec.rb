5d654c2e230cb84f3fe7f57e3a8e58a80b36b8ce
tmd45 <tmd45@otegami.jpn.org>
2015-09-23 15:09:08 +0900

    Fix spec failure using helper method in view
    on rspec-rails 3.x

--- awesome_events/spec/views/events/show.html.erb_spec.rb
+++ awesome_events/spec/views/events/show.html.erb_spec.rb
@@ -3,6 +3,14 @@ require 'rails_helper'
 RSpec.describe "events/show", type: :view do
   context '未ログインユーザがアクセスしたとき' do
     before do
+      def view.logged_in?
+        raise 'Stub #logged_in? if you want to test the behavior.'
+      end
+
+      def view.current_user
+        raise 'Stub #current_user if you want to test the behavior.'
+      end
+
       allow(view).to receive(:logged_in?) { false }
       allow(view).to receive(:current_user) { nil }
     end
