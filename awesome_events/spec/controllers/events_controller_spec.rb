2681b7812f2705e3ee99c4775bc24ab3bf096e9d
tmd45 <tmd45@otegami.jpn.org>
2015-09-23 13:44:57 +0900

    Add spec EventsController#new

--- awesome_events/spec/controllers/events_controller_spec.rb
+++ awesome_events/spec/controllers/events_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  describe 'GET #new' do
+    context '未ログインユーザがアクセスしたとき' do
+      before { get :new }
 
+      it 'トップページにリダイレクトすること' do
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'ログインユーザがアクセスしたとき' do
+      before do
+        user = create :user
+        session[:user_id] = user.id
+        get :new
+      end
+
+      it 'ステータスコード 200 が返ること' do
+        expect(response.status).to eq 200
+      end
+
+      it '@event に新規 Event オブジェクトが格納されていること' do
+        expect(assigns(:event)).to be_a_new(Event)
+      end
+
+      it 'new テンプレートを render していること' do
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
